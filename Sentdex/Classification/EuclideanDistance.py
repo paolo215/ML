42fe1c658aef6c4c86b4be1713804f269a98fb17
paolo215 <paolo_villanueva215@yahoo.com>
2016-11-06 16:19:41 -0500

    Finished K-Nearest Neighbors algorithm p17

--- Sentdex/Classification/EuclideanDistance.py
+++ Sentdex/Classification/EuclideanDistance.py
@@ -29,12 +29,27 @@ def k_nearest_neighbors(data, predict, k=3):
 	if len(data) >= k:
 		warnings.warn("K is set to a value less than total voting goups!")
 
+	distances = []
+	for group in data:
+		for features in data[group]:
+			# euclidean_distance = np.sqrt(np.sum((np.array(features) - np.array(predict))**2))
+			euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
+			distances.append([euclidean_distance, group])
+
+	votes = [i[1] for i in sorted(distances)[:k]]
 	
+
+	vote_result = Counter(votes).most_common(1)[0][0]
+
+
+
 	return vote_result
 
 
 
+result = k_nearest_neighbors(dataset, new_features, k=3)
 
+print result
 
 
 
