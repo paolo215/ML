0cd944bab46a782e43d7254983777d1b14f56bad
paolo215 <paolo_villanueva215@yahoo.com>
2016-11-06 17:32:36 -0500

    Finished K-Nearest Neighbors p19

--- Sentdex/Classification/EuclideanDistance.py
+++ Sentdex/Classification/EuclideanDistance.py
@@ -40,19 +40,22 @@ def k_nearest_neighbors(data, predict, k=3):
 			euclidean_distance = np.linalg.norm(np.array(features) - np.array(predict))
 			distances.append([euclidean_distance, group])
 
+
+
 	votes = [i[1] for i in sorted(distances)[:k]]
 	
-
+	
 	vote_result = Counter(votes).most_common(1)[0][0]
+	confidence = Counter(votes).most_common(1)[0][1] / float(k)
 
-
-
-	return vote_result
+	return vote_result, confidence
 
 
 
 # result = k_nearest_neighbors(dataset, new_features, k=3)
 # print result
+
+
 
 df = pd.read_csv("breast-cancer-wisconsin.data")
 df.replace("?", -99999, inplace=True)
@@ -89,13 +92,12 @@ total = 0
 # Evaluate
 for group in test_set:
 	for data in test_set[group]:
-		vote = k_nearest_neighbors(train_set, data, k=5)
+		vote, confidence = k_nearest_neighbors(train_set, data, k=5)
 		if group == vote:
 			correct += 1
 		total += 1
 
 print "Accuracy: ", float(correct)/total
-
 
 
 
