9c083391547f9733f0310c9cfc8f10aaff29e338
paolo215 <paolo_villanueva215@yahoo.com>
2017-04-02 11:48:58 -0700

    Finished SVM - ud120

--- Udacity/IntroToML/ud120-projects/svm/svm_author_id.py
+++ Udacity/IntroToML/ud120-projects/svm/svm_author_id.py
@@ -24,6 +24,39 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+#clf = SVC(kernel="linear")
+#clf = SVC(kernel="rbf")
+
+clf10 = SVC(kernel="rbf", C=10.0)
+clf100 = SVC(kernel="rbf", C=100.0)
+clf1000 = SVC(kernel="rbf", C=1000.0)
+clf10000 = SVC(kernel="rbf", C=10000.0)
+
+
+#features_train = features_train[:len(features_train) / 100]
+#labels_train = labels_train[:len(labels_train) / 100]
+
+#clf10.fit(features_train, labels_train)
+#clf100.fit(features_train, labels_train)
+#clf1000.fit(features_train, labels_train)
+clf10000.fit(features_train, labels_train)
+
+
+# Output accuracy
+#print("C=10", clf10.score(features_test, labels_test))
+#print("C=100", clf100.score(features_test, labels_test))
+#print("C=1000", clf1000.score(features_test, labels_test))
+#print("C=10000", clf10000.score(features_test, labels_test))
+
+# Give predictions for element 10, 26, 50
+pred10000 = clf10000.predict(features_test)
+#print("10", pred10000[10])
+#print("26", pred10000[26])
+#print("50", pred10000[50])
+
+# How many predicdted to be in 1 class
+print(pred10000[pred10000 == 1].size)
 
 #########################################################
 
