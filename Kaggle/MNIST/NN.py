760191a9c70196b8f6c22667e6772bfa33ca9a86
paolo215 <paolo_villanueva215@yahoo.com>
2017-05-13 02:47:50 -0400

    Updated script for MNIST

--- Kaggle/MNIST/NN.py
+++ Kaggle/MNIST/NN.py
@@ -1,4 +1,5 @@
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 
@@ -6,17 +7,20 @@ import pandas as pd
 df_train = pd.read_csv("train.csv")
 df_test = pd.read_csv("test.csv")
 
-label_column = df_train.columns[len(df_train.columns) - 1]
+label_column = df_train.columns[0]
 
 X_train = df_train.drop(label_column, axis=1)
 Y_train = df_train[label_column]
 
-clf = MLPClassifier(hidden_layer_sizes=(25,), solver="lbfgs", alpha=1e-4)
+clf = MLPClassifier(hidden_layer_sizes=(100,), solver="sgd", alpha=1e-4)
+# clf = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=2, min_samples_leaf=1)
+
 
 clf.fit(X_train, Y_train)
-predictions = clf.predict(df_test)
 
-print(clf.score(X_train, Y_train))
+print(Y_train)
+
+predictions = clf.predict(df_test)
 
 
 log = open("answer.csv", "w")
